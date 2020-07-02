53ddde3724ceeb5ea62ffe7346d2618d6158088d
grace-mugoiri <gracemugoiri@gmail.com>
2020-07-02 20:26:08 +0300

    add delete functionality with axios on client side

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -44,6 +44,16 @@ class IdeasContainer extends Component {
 			})
 		.catch(error => console.log(error))
 	}
+
+	deleteIdea = (id) => {
+		axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+			.then(response => {
+				const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+				const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+				this.setState({ideas: ideas})
+			})
+			.catch(error => console.log(error))
+	}
 	updateIdea = (idea) => {
 		const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
 		const ideas = update(this.state.ideas, {
